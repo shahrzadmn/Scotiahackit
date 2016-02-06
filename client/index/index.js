4fe2a70b3a84c0268d809adb9004a47a4ff7a0e5
Shahrzad Masoumnia <shahrzadmn@Shahrzads-MacBook-Pro.local>
2016-02-05 20:21:19 -0500

    progress check

---
+++ client/index/index.js
@@ -0,0 +1,25 @@
+Template.index.onCreated(function() {
+  // We can use the `ready` callback to interact with the map API once the map is ready.
+  GoogleMaps.ready('map', function(map) {
+    // Add a marker to the map once it's ready
+    var marker = new google.maps.Marker({
+      position: map.options.center,
+      map: map.instance
+    });
+  });
+});
+
+Template.index.onRendered(function() {
+  GoogleMaps.load();
+});
+
+Template.index.helpers({
+  mapOptions: function() {
+    if (GoogleMaps.loaded()) {
+      return {
+        center: new google.maps.LatLng(-37.8136, 144.9631),
+        zoom: 8
+      }
+    }
+  }
+});
