765a4bf3bd0695222543b57056104453a23f2451
Shahrzad Masoumnia <shahrzadmn@Shahrzads-MacBook-Pro.local>
2016-02-06 12:32:58 -0500

    no more homes in water!

--- client/index/index.js
+++ client/index/index.js
@@ -3,42 +3,50 @@ Template.index.onCreated(function() {
   GoogleMaps.ready('map', function(map) {
     Meteor.call('getHomes', function(err, res) {
       if (!err) {
+
+        let elevator = new google.maps.ElevationService;
+        stashInfo = [];
+
         $.each(res, function(index, value) {
-          let marker = new google.maps.Marker({
-            position: { lat: value.lat, lng: value.lng },
-            map: map.instance
-          });
 
-          let generateInfoWindow = (content) => {
-            let infowindow = new google.maps.InfoWindow({
-              content: content
-            });
-            return infowindow;
-          }
-          
-          stashInfo = [];
+          elevator.getElevationForLocations({
+            "locations": [{ lat: value.lat, lng: value.lng }],
+          }, function(results, status) {
+            let checkWater = results[0].elevation;
 
-          marker.addListener('mouseover', function(event) {
-            let info = generateInfoWindow("test");
-            stashInfo.push(info);
-            info.open(map.instance, marker);
+            if (checkWater > 0) {
+              let marker = new google.maps.Marker({
+                position: { lat: value.lat, lng: value.lng },
+                map: map.instance
+              });
 
-            let lat = marker.internalPosition.lat();
-            let long = marker.internalPosition.lng();
-           
-            let elevator = new google.maps.ElevationService;
-            elevator.getElevationForLocations({
-              "locations": [new google.maps.LatLng(lat, long)]
-            }, function(results, status) {
-              console.log(results[0].elevation, status);
-            })
-          });
+              let generateInfoWindow = (content) => {
+                let infowindow = new google.maps.InfoWindow({
+                  content: content
+                });
+                return infowindow;
+              }
 
-          marker.addListener('mouseout', function() {
-            let info = stashInfo.shift();
-            info.close(map.instance, marker);
-          });
+              marker.addListener('mouseover', function(event) {
+                let lat = marker.internalPosition.lat();
+                let long = marker.internalPosition.lng();
+                
+                elevator.getElevationForLocations({
+                  "locations": [new google.maps.LatLng(lat, long)]
+                }, function(results, status) {
+                  let info = generateInfoWindow((results[0].elevation).toString());
+                  stashInfo.push(info);
+                  info.open(map.instance, marker);
+                })
+              });
 
+              marker.addListener('mouseout', function() {
+                let info = stashInfo.shift();
+                info.close(map.instance, marker);
+              });  
+            }
+
+          })
         })
       }
     });
