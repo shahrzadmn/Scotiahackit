90fc8bedc9ab7066f108876059ccc307cbc319f0
Shahrzad Masoumnia <shahrzadmn@Shahrzads-MacBook-Pro.local>
2016-02-05 21:02:37 -0500

    progress check

--- both/routes.js
+++ both/routes.js
@@ -3,3 +3,21 @@ Router.route('/', {
 	layoutTemplate: "applicationLayout",
   loadingTemplate: "loading"
 });
+
+Router.route('/login', {
+  template: "login",
+  layoutTemplate: "defaultLayout",
+  name: "login"
+});
+
+Router.onBeforeAction(function() {
+  if (!Meteor.userId()) {
+    // if the user is not logged in, render the Login template
+    Router.go('login');
+  } else {
+    // otherwise don't hold up the rest of hooks or our route/action function
+    // from running
+    this.next();
+  }
+  this.next();
+});
---
+++ server/boot.js
@@ -0,0 +1,11 @@
+Meteor.startup(function() {
+    ServiceConfiguration.configurations.remove({
+        service: "facebook"
+    });
+
+   ServiceConfiguration.configurations.insert({
+        service: "facebook",
+        appId: Meteor.settings.facebook.appId,
+        secret: Meteor.settings.facebook.secret
+    });
+})
