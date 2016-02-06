371908b47e1fe730bfa047de3d451db6fd529bf7
Shahrzad Masoumnia <shahrzadmn@Shahrzads-MacBook-Pro.local>
2016-02-06 14:02:45 -0500

    updated input fields instead of spans

--- client/layouts/applicationLayout/applicationLayout.js
+++ client/layouts/applicationLayout/applicationLayout.js
@@ -2,8 +2,7 @@ Template.applicationLayout.onRendered(function() {
   $('.ui.sidebar').sidebar();
   $('.ui.dropdown').dropdown();
   $('.ui.accordion').accordion();
-  configureSliders();  
-
+  configureSliders(Meteor.user());
 });
 
 Template.applicationLayout.events({
@@ -12,6 +11,31 @@ Template.applicationLayout.events({
   },
   'click [role="logout"]': (event) => {
     AccountsTemplates.logout();
+  },
+  'click #submitBasicSettings': (event) => {
+    let settings = {
+      salary: $('#salary').val(),
+      debt: $('#debt').val(),
+      expenses: $('#expenses').val()
+    }
+
+    Meteor.call('saveBasicSettings', settings, Meteor.userId(), function(err, res) {
+      if (!err) {
+        toastr["info"]("Saved your basic settings!")
+      }
+    })
+  },
+  'click #submitAdvancedSettings': (event) => {
+    let settings = {
+      amortization: $('#amortization').val(),
+      interestRate: $('#interest-rate').text()
+    }
+
+    Meteor.call('saveAdvancedSettings', settings, Meteor.userId(), function(err, res) {
+      if (!err) {
+        toastr["success"]("Saved your advanced settings!")
+      }
+    })
   }
 })
 
@@ -71,30 +95,39 @@ Template.applicationLayout.helpers({
 })
 
 
-function configureSliders() {
+function configureSliders(user) {
   // salary
   let salarySlider = $('#salary');
   let salaryValue = $('#salary--value');
 
   salarySlider.on('input change', function(event) {
-    salaryValue.html("$" + salarySlider.val());
+    salaryValue.val(salarySlider.val());
   }); 
+
+  salarySlider.val(user.profile.basicSettings.salary);
+  salaryValue.val(user.profile.basicSettings.salary);
 
   // debt
   let debtSlider = $('#debt');
   let debtValue = $('#debt--value');
 
   debtSlider.on('input change', function(event) {
-    debtValue.html("$" + debtSlider.val());
+    debtValue.val(debtSlider.val());
   }); 
+
+  debtSlider.val(user.profile.basicSettings.debt);
+  debtValue.val(user.profile.basicSettings.debt);
 
   // expenses
   let expensesSlider = $('#expenses');
   let expensesValue = $('#expenses--value');
 
   expensesSlider.on('input change', function(event) {
-    expensesValue.html("$" + expensesSlider.val());
+    expensesValue.val(expensesSlider.val());
   });
+
+  expensesSlider.val(user.profile.basicSettings.expenses);
+  expensesValue.val(user.profile.basicSettings.expenses);
 
   // amortization
   let amortizationSlider = $('#amortization');
@@ -104,5 +137,29 @@ function configureSliders() {
     amortizationValue.html(amortizationSlider.val() + " years");
   });
 
+  // common event handler for all sliders
+  // $(document).on('input change', [salarySlider, debtSlider, expensesSlider], function(event) {
+  //   let salary = salarySlider.val();
+  //   let debt = debtSlider.val();
+  //   let expenses = expensesSlider.val();
+  //   console.log(salary, debt, expenses);
+  // });
+}
 
+toastr.options = {
+  "closeButton": false,
+  "debug": false,
+  "newestOnTop": false,
+  "progressBar": false,
+  "positionClass": "toast-bottom-right",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "5000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
 }
--- server/methods.js
+++ server/methods.js
@@ -1,5 +1,21 @@
 Meteor.methods({
   'getHomes': function() {
     return Homes.find({}).fetch()
+  },
+  'saveBasicSettings': function(settings, userId) {
+    Meteor.users.update(userId, {
+      $set: {
+        "profile.basicSettings": settings
+      }
+    });
+    return true;
+  },
+  'saveAdvancedSettings': function(settings, userId) {
+    Meteor.users.update(userId, {
+      $set: {
+        "profile.advancedSettings": settings
+      }
+    });
+    return true;
   }
 });
